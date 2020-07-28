7593114720101bf5c58beb04ac0156117c74990c
Chester Cabahug <chesterdeveloper2000@gmail.com>
2020-07-28 12:48:19 +0800

    added 20 bombs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid")
     let width = 10
+    let bombAmount = 20
     let squares = []
 
     // create board
     createBoard = () => {
+        // get shuffled game array with random bombs
+        const bombArray = Array(bombAmount).fill("bomb")
+        const emptyArray = Array(width*width - bombAmount).fill("valid")
+        console.log(bombArray)
+        console.log(emptyArray)
+
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement("div")
             square.setAttribute("id", i)
