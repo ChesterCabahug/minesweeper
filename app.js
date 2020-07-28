c9fab6a8f84e1368285bc6cdc27d3a4fa0a4194a
Chester <chesterdeveloper2000@gmail.com>
2020-07-28 13:00:57 +0800

    joining the 80 empty bomb with the 20 bombs randomly

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(bombArray)
         console.log(emptyArray)
 
+        const gameArray = emptyArray.concat(bombArray)
+        console.log(gameArray)
+
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
+        console.log(shuffledArray)
+
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement("div")
             square.setAttribute("id", i)
